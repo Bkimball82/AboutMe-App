cc71c70a188a2972a3c97c698754b428f197a51e
Brandon Kimball <Brandon.Kimball82@gmail.com>
2019-10-21 17:43:50 -0500

    Set up the Contact Page and Finished up the Main Page

--- AboutMe/AboutMe/ContactPage.xaml.cs
+++ AboutMe/AboutMe/ContactPage.xaml.cs
@@ -16,5 +16,37 @@ namespace AboutMe
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (nameEntry.Text == "" || nameEntry.Text == null)
+            {
+                await DisplayAlert("Error!", "Please Enter a Name", "OK");
+                return;
+            }
+
+            if (emailEntry.Text == "" || emailEntry.Text == null)
+            {
+                await DisplayAlert("Error!", "Please Enter a Email", "OK");
+                return;
+            }
+
+            if (phoneEntry.Text == "" || phoneEntry.Text == null)
+            {
+                await DisplayAlert("Error!", "Please Enter a Phone Number", "OK");
+                return;
+            }
+
+            if (questionEntry.Text == "" || questionEntry.Text == null)
+            {
+                await DisplayAlert("Error!", "Please Enter a Question", "OK");
+                return;
+            }
+
+
+            await DisplayAlert("Thanks", "Your message was sent", "OK");
+
+            Navigation.PopAsync();
+        }
     }
 }
