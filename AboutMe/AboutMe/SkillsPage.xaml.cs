b8a83813c82c861ce912aa21492ea1748f200d9e
Brandon Kimball <Brandon.Kimball82@gmail.com>
2019-10-23 17:53:23 -0500

    Added about me page and skills page to app

--- AboutMe/AboutMe/SkillsPage.xaml.cs
+++ AboutMe/AboutMe/SkillsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,21 @@ namespace AboutMe
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SkillsPage : ContentPage
     {
+        public ObservableCollection<String> Skills { get; set; }
         public SkillsPage()
         {
             InitializeComponent();
+
+            this.Skills = new ObservableCollection<string>();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            this.Skills.Add(newSkillEntry.Text);
+
+            skillsList.ItemsSource = this.Skills;
+
+            newSkillEntry.Text = "";
         }
     }
 }
